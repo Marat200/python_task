93a7b8b6476e6e753bf6d1dfa1fc4345db1964de
Marat <marat07052002@inbox.ru>
2020-11-25 16:16:37 +0500

    Задания к python

---
+++ home work1/work1.py
@@ -0,0 +1,12 @@
+name = 'Bruce'
+surname = 'Wayne'
+age = 32
+
+print(name, surname, age)
+
+name = str(input('Введите свое имя: '))
+surname = str(input('Введите свою фамилию: '))
+age = int(input('Введите свой возраст: '))
+
+
+print(f'Ваше имя: {name}\nВаша фамилия: {surname}\nВаш возраст: {age}')
