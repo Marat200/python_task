a2662aa0f51f2ca3fa783ccb6e336e9077fe12e0
Marat_Galimov <72463817+Marat200@users.noreply.github.com>
2020-12-07 20:10:38 +0500

    Add files via upload

---
+++ задание 2 к уроку 3.py
@@ -0,0 +1,7 @@
+def data(name, surname, year, city, email, phone):
+    print(
+        f'Name: {name}; Surname: {surname}; Birth year: {year};'
+        f'City of living: {city}; Email: {email}; Phone number: {phone}')
+
+
+data(name='Bruce', surname='Wayne', year=1978, city='Gotham City', email='brucewayne@gmail.com', phone='+79354466000')
