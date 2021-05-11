9468c5f2984740659950371dc39c3eab2613ba17
Marat_Galimov <72463817+Marat200@users.noreply.github.com>
2021-05-11 11:01:28 +0500

    Add files via upload

--- задание 1 к уроку 6.py
+++ задание 1 к уроку 6.py
@@ -1,22 +1,39 @@
-from time import sleep
+'''
+x64 windows
+Python 3.8
+'''
+'''
+Размер листа больше, чем неизменяемый кортеж, с одним набором данных
+(912 байт у листа 848 байт у кортежа). Размер не зависит от разрядности чисел.
+Зависит от размера массива.
+Каждый элемент массива занимает 28 байт. 100 элементов по 28 байт занимают
+2800 байт. Это значительно больше размера массива этих данных, т.к. Python
+делает оптимизацию и создаёт ссылки на один и тот же объект.
+'''
 
+import random
+import sys
 
-class TrafficLight:
-    __color = ['Красный', 'Желтый', 'Зеленый']
+r = [random.randint(0, 99) for _ in range(100)]
+print(f'Массив: {r}')
 
-    def running(self):
-        i = 0
-        while i < 3:
-            print(f'Светофор переключается \n '
-                  f'{TrafficLight.__color[i]}')
-            if i == 0:
-                sleep(7)
-            elif i == 1:
-                sleep(5)
-            elif i == 2:
-                sleep(3)
-            i += 1
+min_index_1 = 0
+min_index_2 = 1
 
+for i in r:
+    if r[min_index_1] > i:
+        min_index_2 = min_index_1
+        min_index_1 = r.index(i)
+    elif r[min_index_2] > i:
+        min_index_2 = r.index(i)
 
-TrafficLight = TrafficLight()
-TrafficLight.running()
+print(f'Два наименьших элемента: {r[min_index_1]} и {r[min_index_2]}')
+
+print('Размер листа', sys.getsizeof(r))
+print('Размер элемента листа', sys.getsizeof(r[0]))
+print('Размер кортежа', sys.getsizeof(tuple(r)))
+print('Размер элемента кортежа', sys.getsizeof(tuple(r)[0]))
+sum = 0
+for size in r:
+    sum += sys.getsizeof(size)
+print('Размер всех элементов в листе', sum)
